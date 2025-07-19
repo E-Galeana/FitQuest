215699cee29a0338e28f8c5cf553f3be713e81f1
Emanuel Galeana <egaleanam@protonmail.com>
2025-07-18 21:54:00 -0400

    Created calendar page (By Raumsie)

---
+++ lib/screens/calendar_page.dart
@@ -0,0 +1,21 @@
+// calendar_page.dart
+import 'package:flutter/material.dart';
+
+class CalendarPage extends StatelessWidget {
+  const CalendarPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calendar'),
+      ),
+      body: const Center(
+        child: Text(
+          'Calendar View - Coming Soon!',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
